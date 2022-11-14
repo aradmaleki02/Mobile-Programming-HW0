e15348f2e0d7c5320b05e16893705fe7bebd5f83
Arad Maleki <arad.m.t02@gmail.com>
2022-11-15 01:36:21 +0330

    Added night mode

--- app/src/main/java/com/aradmaleki/myresume/MainActivity.java
+++ app/src/main/java/com/aradmaleki/myresume/MainActivity.java
@@ -1,11 +1,18 @@
 package com.aradmaleki.myresume;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwitchMaterial switchMaterial = findViewById(R.id.switch_button);
+        TextView textView = findViewById(R.id.switch_button_text);
+        CircleImageView circleImageView = findViewById(R.id.profile_image);
+
+        switchMaterial.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                textView.setText(getString(R.string.dark_mode));
+                circleImageView.setBorderColor(Color.WHITE);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                textView.setText(getString(R.string.light_mode));
+                circleImageView.setBorderColor(Color.BLACK);
+            }
+        });
     }
 
     public void callMe(View view) {
@@ -21,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
         intent.setData(Uri.parse(phoneNumber));
         startActivity(intent);
     }
+
 }
